62dd6b96b64060362eea1b085c8478bfa570e9c2
Lee Clement <clement.leopold@gmail.com>
2016-10-17 22:37:18 +1100

    Got simple pose relaxation problem working. Weird factor of two on the update though. Need to investigate.

--- pose_to_pose_test.py
+++ pose_to_pose_test.py
@@ -5,19 +5,26 @@ from problem import Problem
 from liegroups import SE3, SO3
 
 T_1_0 = SE3.identity()
-T_2_0 = SE3(SO3.identity(), np.array([0.1, 0, 0]))
+T_2_0 = SE3(SO3.identity(), np.array([0.13, 0.0, 0.0]))
+T_3_0 = SE3(SO3.identity(), np.array([0.16, 0.0, 0.0]))
 
 T_1_0_obs = SE3.identity()
-T_2_1_obs = T_2_0 * T_1_0.inv()
+T_2_1_obs = SE3(SO3.identity(), np.array([0.1, 0, 0]))
+T_3_2_obs = SE3(SO3.identity(), np.array([0.1, 0, 0]))
 
-
+# Either we need a prior on the first pose, or it needs to be held constant
+# so that the resulting system of linear equations is solveable
 cost0 = SE3Cost(T_1_0_obs, np.linalg.inv(1e-12 * np.identity(6)))
 cost0_params = [T_1_0]
 
 cost1 = SE3toSE3Cost(T_2_1_obs, np.linalg.inv(1e-3 * np.identity(6)))
 cost1_params = [T_1_0, T_2_0]
 
+cost2 = SE3toSE3Cost(T_3_2_obs, np.linalg.inv(1e-3 * np.identity(6)))
+cost2_params = [T_2_0, T_3_0]
+
 problem = Problem()
 problem.add_residual_block(cost0, cost0_params)
 problem.add_residual_block(cost1, cost1_params)
+problem.add_residual_block(cost2, cost2_params)
 problem.solve()
--- problem.py
+++ problem.py
@@ -20,15 +20,47 @@ class Problem:
         all_params = [
             i for i in itertools.chain.from_iterable(self.block_params)]
         unique_params = self._get_unique(all_params)
+        update_ranges = []
+        for p in unique_params:
+            if not update_ranges:
+                update_ranges.append(range(p.dof))
+            else:
+                update_ranges.append(range(
+                    update_ranges[-1].stop,
+                    update_ranges[-1].stop + p.dof))
 
-        dx = self.solve_one_iter(unique_params)
-        print(dx)
+        max_iters = 10
+
+        num_iters = 0
+        dx = np.array([100])
+        prev_cost = np.inf
+        cost = np.inf
+        while num_iters < max_iters and \
+                np.linalg.norm(dx) > 1e-2 and \
+                cost <= prev_cost:
+            prev_cost = cost
+
+            num_iters += 1
+            print("iter = %d" % num_iters)
+
+            cost, dx = self.solve_one_iter(unique_params)
+            print("Cost: %f" % cost)
+            print("Update vector:\n", str(dx))
+            print("Norm = %f" % np.linalg.norm(dx))
+
+            for p, r in zip(unique_params, update_ranges):
+                print("Before:\n", str(p))
+                # TODO: Figure out where this factor of 2 comes from
+                p.perturb(0.5 * dx[r])
+                print("After:\n", str(p))
+            print()
 
     def solve_one_iter(self, unique_params):
         b_blocks = [[None] for _ in range(len(self.residual_blocks))]
         A_blocks = [[None for _ in range(len(unique_params))]
                     for _ in range(len(self.residual_blocks))]
 
+        cost = 0
         block_ridx = 0
         for block, params in zip(self.residual_blocks, self.block_params):
 
@@ -36,25 +68,31 @@ class Problem:
 
             for par, jac in zip(params, jacobians):
                 jac_times_weight = jac.T.dot(block.weight)
+                cost += residual.dot(block.weight.dot(residual))
 
                 block_cidx = unique_params.index(par)
 
-                A_blocks[block_ridx][block_cidx] = sparse.coo_matrix(
+                # Not sure if CSR or CSC is the best choice here.
+                # spsolve requires one or the other
+                # TODO: Check timings for both
+                A_blocks[block_ridx][block_cidx] = sparse.csr_matrix(
                     jac_times_weight.dot(jac))
 
                 if b_blocks[block_ridx][0] is None:
-                    b_blocks[block_ridx][0] = sparse.coo_matrix(
+                    b_blocks[block_ridx][0] = sparse.csr_matrix(
                         jac_times_weight.dot(residual)).T
                 else:
-                    b_blocks[block_ridx][0] += sparse.coo_matrix(
+                    b_blocks[block_ridx][0] += sparse.csr_matrix(
                         jac_times_weight.dot(residual)).T
 
             block_ridx += 1
 
-        A = sparse.csr_matrix(sparse.bmat(A_blocks))
-        b = sparse.csr_matrix(sparse.bmat(b_blocks))
+        A = sparse.bmat(A_blocks, format='csr')
+        b = sparse.bmat(b_blocks, format='csr')
 
-        return splinalg.spsolve(A, b)
+        dx = splinalg.spsolve(A, b)
+
+        return 0.5 * cost, dx
 
     def _get_unique(self, seq, idfun=None):
         if idfun is None:
