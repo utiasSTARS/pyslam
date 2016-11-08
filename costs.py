Fixed error computations and jacobians, plus missing negative sign in solve. Works with static noise now.

costs.py
@@ -12,13 +12,14 @@ class SE3Cost:
 
     def evaluate(self, params, compute_jacobians=None):
         T_est = params[0]
-        residual = SE3.log(self.T_obs * T_est.inv())
+
+        residual = SE3.log(T_est * self.T_obs.inv())
 
         if compute_jacobians:
             jacobians = [None for _ in range(len(params))]
 
             if compute_jacobians[0]:
-                jacobians[0] = self.T_obs.adjoint()
+                jacobians[0] = T_est.adjoint()
 
             return residual, jacobians
 
@@ -36,15 +37,15 @@ class SE3toSE3Cost:
         T_1_0 = params[0]
         T_2_0 = params[1]
 
-        T_1_2_est = T_1_0 * T_2_0.inv()
+        T_2_1_est = T_2_0 * T_1_0.inv()
 
-        residual = SE3.log(self.T_2_1_obs * T_1_2_est)
+        residual = SE3.log(T_2_1_est * self.T_2_1_obs.inv())
 
         if compute_jacobians:
             jacobians = [None for _ in range(len(params))]
 
             if compute_jacobians[0]:
-                jacobians[0] = T_1_2_est.inv().adjoint()
+                jacobians[0] = T_2_1_est.adjoint()
 
             if compute_jacobians[1]:
                 jacobians[1] = np.identity(6)

problem.py
@@ -13,9 +13,12 @@ class Options:
 
     def __init__(self):
         self.max_iters = 100
-        self.min_update_norm = 1e-2
+        self.min_update_norm = 1e-4
         self.min_cost = 1e-12
-        self.line_search_alpha = 0.5
+
+        self.linesearch_alpha = 0.8
+        self.linesearch_max_iters = 10
+        self.linesearch_min_cost_decrease = 0.9
 
 
 class Problem:
@@ -23,10 +26,18 @@ class Problem:
 
     def __init__(self, options=Options()):
         self.options = options
+        """Optimization options."""
+
+        self.param_list = []
+        """List of all parameters to be optimized."""
 
         self.residual_blocks = []
-        self.param_list = []
+        """List of residual blocks."""
         self.block_param_ids = []
+        """Indices of parameters in self.param_list that each block depends on."""
+
+        self.constant_params = []
+        """List of parameters to be held constant."""
 
     def add_residual_block(self, block, params):
         self.residual_blocks.append(block)
@@ -38,6 +49,16 @@ class Problem:
             param_ids.append(self.param_list.index(p))
 
         self.block_param_ids.append(param_ids)
+
+    def set_parameter_blocks_constant(self, params):
+        for p in params:
+            if p not in self.constant_params:
+                self.constant_params.append(p)
+
+    def set_parameter_blocks_variable(self, params):
+        for p in params:
+            if p in self.constant_params:
+                self.constant_params.remove(p)
 
     def solve(self):
         update_ranges = []
@@ -63,7 +84,7 @@ class Problem:
             print("iter = %d" % num_iters)
 
             dx = self.solve_one_iter()
-            # print("Update vector:\n", str(dx))
+            print("Update vector:\n", str(dx))
             print("Update norm = %f" % np.linalg.norm(dx))
 
             # Backtrack line search
@@ -71,8 +92,10 @@ class Problem:
             best_step_size = step_size
             best_cost = np.inf
             search_done = False
+            ls_iters = 0
 
             while not search_done:
+                ls_iters += 1
                 test_params = copy.deepcopy(self.param_list)
 
                 for p, r in zip(test_params, update_ranges):
@@ -80,13 +103,19 @@ class Problem:
 
                 test_cost = self.eval_cost(test_params)
                 print(step_size, " : ", test_cost)
-                if test_cost < best_cost:
+                if ls_iters < self.options.linesearch_max_iters and \
+                        test_cost < \
+                        self.options.linesearch_min_cost_decrease * best_cost:
                     best_cost = test_cost
                     best_step_size = step_size
                 else:
+                    if test_cost < best_cost:
+                        best_cost = test_cost
+                        best_step_size = step_size
+
                     search_done = True
 
-                step_size = self.options.line_search_alpha * step_size
+                step_size = self.options.linesearch_alpha * step_size
 
             print("Best step size: %f" % best_step_size)
             print("Best cost: %f" % best_cost)
@@ -125,10 +154,10 @@ class Problem:
 
                 if b_blocks[block_ridx][0] is None:
                     b_blocks[block_ridx][0] = sparse.csr_matrix(
-                        jac_times_weight.dot(residual)).T
+                        -jac_times_weight.dot(residual)).T
                 else:
                     b_blocks[block_ridx][0] += sparse.csr_matrix(
-                        jac_times_weight.dot(residual)).T
+                        -jac_times_weight.dot(residual)).T
 
             block_ridx += 1
 
@@ -145,6 +174,7 @@ class Problem:
         for block, pids in zip(self.residual_blocks, self.block_param_ids):
             params = [param_list[pid] for pid in pids]
             residual = block.evaluate(params)
+            # import pdb; pdb.set_trace()
             cost += residual.dot(block.weight.dot(residual))
 
         return 0.5 * cost

relaxation_test.py
@@ -6,19 +6,20 @@ from problem import Problem, Options
 
 T_1_0_obs = SE3.identity()
 T_2_1_obs = SE3.exp(
-    np.array([0.1, -0.1, 0.1, 0, 0, 0]))
+    np.array([0.1, -0.1, 0.1, -0.1, 0.1, -0.1]))
 T_3_2_obs = SE3.exp(
-    np.array([0.1, -0.1, 0.1, 0, 0, 0]))
+    np.array([0.1, -0.1, 0.1, -0.1, 0.1, -0.1]))
 
 T_1_0_est = SE3.identity()
 T_2_0_est = T_2_1_obs * T_1_0_obs
 T_3_0_est = T_3_2_obs * T_2_1_obs * T_1_0_obs
 
-T_1_0_est = SE3.exp(0.2 * np.random.rand(6)) * T_1_0_est
-T_2_0_est = SE3.exp(0.2 * np.random.rand(6)) * T_2_0_est
-T_3_0_est = SE3.exp(0.2 * np.random.rand(6)) * T_3_0_est
-# T_2_0_est = SE3.exp(np.array([0.01, 0.01, 0.01, 0, 0, 0])) * T_2_0_est
-# T_3_0_est = SE3.exp(np.array([0.01, 0.01, 0.01, 0, 0, 0])) * T_3_0_est
+# T_1_0_est = SE3.exp(0.2 * np.random.rand(6)) * T_1_0_est
+# T_2_0_est = SE3.exp(0.2 * np.random.rand(6)) * T_2_0_est
+# T_3_0_est = SE3.exp(0.2 * np.random.rand(6)) * T_3_0_est
+T_1_0_est = SE3.exp(np.array([0.1, 0.1, 0.1, 0.1, 0.1, 0.1])) * T_1_0_est
+T_2_0_est = SE3.exp(np.array([0.1, -0.1, 0.1, -0.1, 0.1, -0.1])) * T_2_0_est
+T_3_0_est = SE3.exp(np.array([-0.1, 0.1, -0.1, 0.1, -0.1, 0.1])) * T_3_0_est
 
 # Either we need a prior on the first pose, or it needs to be held constant
 # so that the resulting system of linear equations is solveable
@@ -37,4 +38,9 @@ problem = Problem(options)
 problem.add_residual_block(cost0, cost0_params)
 problem.add_residual_block(cost1, cost1_params)
 problem.add_residual_block(cost2, cost2_params)
+
+# problem.set_parameter_blocks_constant(cost0_params)
+# print(problem.constant_params)
+# problem.set_parameter_blocks_variable(cost0_params)
+
 problem.solve()