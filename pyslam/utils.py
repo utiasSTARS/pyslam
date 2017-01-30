50413c9d88cc05ed67d0b1e3498166be258b1942
Lee Clement <clement.leopold@gmail.com>
2017-01-30 14:34:43 -0500

    Updated bilinear_interpolate to handle vector inputs and output np.nan for invalid queries

--- pyslam/utils.py
+++ pyslam/utils.py
@@ -5,24 +5,42 @@ def bilinear_interpolate(im, x, y):
     x = np.asarray(x)
     y = np.asarray(y)
 
+    if not x.shape == y.shape:
+        raise ValueError("x and y must have the same shape")
+
+    interpolated = np.empty(x.shape)
+    interpolated.fill(np.nan)
+
     x0 = np.floor(x).astype(int)
     x1 = x0 + 1
     y0 = np.floor(y).astype(int)
     y1 = y0 + 1
 
-    x0 = np.clip(x0, 0, im.shape[1] - 1)
-    x1 = np.clip(x1, 0, im.shape[1] - 1)
-    y0 = np.clip(y0, 0, im.shape[0] - 1)
-    y1 = np.clip(y1, 0, im.shape[0] - 1)
+    # x0 = np.clip(x0, 0, im.shape[1] - 1)
+    # x1 = np.clip(x1, 0, im.shape[1] - 1)
+    # y0 = np.clip(y0, 0, im.shape[0] - 1)
+    # y1 = np.clip(y1, 0, im.shape[0] - 1)
 
-    Ia = im[y0, x0]
-    Ib = im[y1, x0]
-    Ic = im[y0, x1]
-    Id = im[y1, x1]
+    valid_mask = (x0 >= 0) &  (x0 < im.shape[1]) & (x1 >= 0) & (x1 < im.shape[1]) \
+        & (y0 >= 0) & (y0 < im.shape[0]) & (y1 >= 0) & (y1 < im.shape[0])
 
-    wa = (x1 - x) * (y1 - y)
-    wb = (x1 - x) * (y - y0)
-    wc = (x - x0) * (y1 - y)
-    wd = (x - x0) * (y - y0)
+    valid_x = x[valid_mask]
+    valid_x0 = x0[valid_mask]
+    valid_x1 = x1[valid_mask]
+    valid_y = y[valid_mask]
+    valid_y0 = y0[valid_mask]
+    valid_y1 = y1[valid_mask]
 
-    return wa * Ia + wb * Ib + wc * Ic + wd * Id
+    Ia = im[valid_y0, valid_x0]
+    Ib = im[valid_y1, valid_x0]
+    Ic = im[valid_y0, valid_x1]
+    Id = im[valid_y1, valid_x1]
+
+    wa = (valid_x1 - valid_x) * (valid_y1 - valid_y)
+    wb = (valid_x1 - valid_x) * (valid_y - valid_y0)
+    wc = (valid_x - valid_x0) * (valid_y1 - valid_y)
+    wd = (valid_x - valid_x0) * (valid_y - valid_y0)
+
+    interpolated[valid_mask] = wa * Ia + wb * Ib + wc * Ic + wd * Id
+
+    return interpolated
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,7 +5,7 @@ import pyslam.utils
 
 def test_bilinear_interpolate():
     im = np.eye(2)
-    x = 0.5
-    y = 0.5
+    x = [0.5, -1]
+    y = [0.5, 2]
     interpolated = pyslam.utils.bilinear_interpolate(im, x, y)
-    assert(interpolated == 0.5)
+    assert(interpolated[0] == 0.5 and np.isnan(interpolated[1]))
