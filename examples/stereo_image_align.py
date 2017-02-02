4f19ae698026996dae0f72761352c4c2777671a2
Lee Clement <clement.leopold@gmail.com>
2017-02-02 18:32:05 -0500

    Added coarse-to-fine optimization via image pyramids. Converges faster and gets more accurate results.

--- examples/stereo_image_align.py
+++ examples/stereo_image_align.py
@@ -8,6 +8,7 @@ from liegroups import SE3
 from pyslam.problem import Options, Problem
 from pyslam.sensors import StereoCamera
 from pyslam.costs import PhotometricCost
+import time
 
 # Load KITTI data
 basedir = '/Users/leeclement/Desktop/odometry_raw/'
@@ -31,102 +32,89 @@ T_1_0_true = T_1_w * T_0_w.inv()
 
 # params_init = {'T_1_0': T_1_0_true}
 params_init = {'T_1_0': SE3.identity()}
-# params_init = {'T_1_0': SE3.from_matrix(
-#     np.array([[0.99999976,  0.0003423,   0.00060361,  0.01976551],
-#               [-0.00034254,  0.99999987,  0.00038914,  0.0249602],
-#               [-0.00060348, -0.00038935,  0.99999974, - 0.57019808],
-#               [0.,          0.,        0.,      1.]]))}
 
 # Scaling parameters
-pyrlevels = 0
-pyrfactor = 1. / 2.**pyrlevels
+pyrlevels = [3, 2]
 
-# Disparity computation parameters
-window_size = 5
-min_disp = 0
-max_disp = np.max([16, np.int(64 * pyrfactor)]) + min_disp
+params = params_init
 
-# Use semi-global block matching
-stereo = cv2.StereoSGBM_create(
-    minDisparity=min_disp,
-    numDisparities=max_disp - min_disp,
-    blockSize=window_size)
-
-# Use regular block matching
-# stereo = cv2.StereoBM_create(
-#     numDisparities=max_disp - min_disp, blockSize=window_size)
-
-impairs = []
-for ims in dataset.gray:
-    imL = ims.left
-    imR = ims.right
-    for _ in range(pyrlevels):
-        imL = cv2.pyrDown(imL)
-        imR = cv2.pyrDown(imR)
-    impairs.append([imL, imR])
-
-disp = []
-for ims in impairs:
-    d = stereo.compute(ims[0], ims[1])
-    disp.append(d.astype(float) / 16.)
-
-for i, d in enumerate(disp):
-    disp[i][d < min_disp + 1] = np.nan
-    disp[i][d > max_disp] = np.nan
-
-# Compute image jacobians
-im_jac = []
-for ims in impairs:
-    # gradx = cv2.Sobel(ims[0], -1, 1, 0)
-    # grady = cv2.Sobel(ims[0], -1, 0, 1)
-    gradx = cv2.Scharr(ims[0], -1, 1, 0)
-    grady = cv2.Scharr(ims[0], -1, 0, 1)
-    im_jac.append(np.array([gradx.astype(float) / 255.,
-                            grady.astype(float) / 255.]))
-
-# Create the camera
-fu = dataset.calib.K_cam0[0, 0] * pyrfactor
-fv = dataset.calib.K_cam0[1, 1] * pyrfactor
-cu = dataset.calib.K_cam0[0, 2] * pyrfactor
-cv = dataset.calib.K_cam0[1, 2] * pyrfactor
-b = dataset.calib.b_gray
-w = impairs[0][0].shape[1]
-h = impairs[0][0].shape[0]
-camera = StereoCamera(cu, cv, fu, fv, b, w, h)
-
-# Create the cost function
-im_ref = impairs[0][0].astype(float) / 255.
-disp_ref = disp[0]
-im_track = impairs[1][0].astype(float) / 255.
-jac_ref = im_jac[0]
-cost = PhotometricCost(camera, im_ref, disp_ref, jac_ref, im_track, 1.)
-
-import time
-iters = 100
-start = time.perf_counter()
-for _ in range(iters):
-    residual = cost.evaluate([params_init['T_1_0']])
-end = time.perf_counter()
-print("Eval only")
-print("Elapsed time : {} sec".format(end - start))
-print("Average time per iteration: {} sec".format((end - start) / iters))
-
-start = time.perf_counter()
-for _ in range(iters):
-    residual, jacobians = cost.evaluate([params_init['T_1_0']], [True])
-end = time.perf_counter()
-print("Eval + Jacobian")
-print("Elapsed time: {} sec".format(end - start))
-print("Average time per iteration: {} sec".format((end - start) / iters))
-
-# Optimize
 options = Options()
 options.allow_nondecreasing_steps = True
 options.max_nondecreasing_steps = 3
-options.min_cost_decrease = 1.
+options.min_cost_decrease = 0.99
 options.max_iters = 1e6
+# options.print_iter_summary = True
 
-problem = Problem(options)
-problem.add_residual_block(cost, ['T_1_0'])
-problem.initialize_params(params_init)
-params_final = problem.solve()
+for pyrlevel in pyrlevels:
+    pyrfactor = 1. / 2.**pyrlevel
+
+    # Disparity computation parameters
+    window_size = 5
+    min_disp = 1
+    max_disp = np.max([16, np.int(64 * pyrfactor)]) + min_disp
+
+    # Use semi-global block matching
+    stereo = cv2.StereoSGBM_create(
+        minDisparity=min_disp,
+        numDisparities=max_disp - min_disp,
+        blockSize=window_size)
+
+    # Use regular block matching
+    # stereo = cv2.StereoBM_create(
+    #     numDisparities=max_disp - min_disp, blockSize=window_size)
+
+    impairs = []
+    for ims in dataset.gray:
+        imL = ims.left
+        imR = ims.right
+        for _ in range(pyrlevel):
+            imL = cv2.pyrDown(imL)
+            imR = cv2.pyrDown(imR)
+        impairs.append([imL, imR])
+
+    disp = []
+    for ims in impairs:
+        d = stereo.compute(ims[0], ims[1])
+        disp.append(d.astype(float) / 16.)
+
+    for i, d in enumerate(disp):
+        disp[i][d < min_disp + 1] = np.nan
+        disp[i][d > max_disp] = np.nan
+
+    # Compute image jacobians
+    im_jac = []
+    for ims in impairs:
+        # gradx = cv2.Sobel(ims[0], -1, 1, 0)
+        # grady = cv2.Sobel(ims[0], -1, 0, 1)
+        gradx = cv2.Scharr(ims[0], -1, 1, 0)
+        grady = cv2.Scharr(ims[0], -1, 0, 1)
+        im_jac.append(np.array([gradx.astype(float) / 255.,
+                                grady.astype(float) / 255.]))
+
+    # Create the camera
+    fu = dataset.calib.K_cam0[0, 0] * pyrfactor
+    fv = dataset.calib.K_cam0[1, 1] * pyrfactor
+    cu = dataset.calib.K_cam0[0, 2] * pyrfactor
+    cv = dataset.calib.K_cam0[1, 2] * pyrfactor
+    b = dataset.calib.b_gray
+    w = impairs[0][0].shape[1]
+    h = impairs[0][0].shape[0]
+    camera = StereoCamera(cu, cv, fu, fv, b, w, h)
+
+    # Create the cost function
+    im_ref = impairs[0][0].astype(float) / 255.
+    disp_ref = disp[0]
+    im_track = impairs[1][0].astype(float) / 255.
+    jac_ref = im_jac[0]
+    cost = PhotometricCost(camera, im_ref, disp_ref, jac_ref, im_track, 1.)
+
+    # Optimize
+    start = time.perf_counter()
+
+    problem = Problem(options)
+    problem.add_residual_block(cost, ['T_1_0'])
+    problem.initialize_params(params)
+    params = problem.solve()
+
+    end = time.perf_counter()
+    print(end - start)
